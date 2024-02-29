60d45083a94047b6dc148e45f1d9040d55268a1b
Koreoxy <rayssankn@gmail.com>
2024-02-29 15:07:34 +0700

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,202 @@
 import { Section } from "./components/site/Section";
 import { Container } from "./components/site/Container";
 import { Layout } from "./components/site/Layout";
+import { Button } from "./components/library/Button";
 
 function App() {
   return (
     <Layout>
       <Container
-        title="Button component"
-        description="ini adalah button component ui"
+        title="Button"
+        description="a button is a widget enalbe users to trigger or event, such as submiiting a form, opening dialog, acncelling an action, or performing a delete operation"
       >
+        {/* BUTTON PRIMARY */}
         <Section
-          title="NEW Section heading"
-          description="Section description text"
+          title="Primary"
+          description="Primary actions should be obvious, solid, high contract backgeound colors work get her"
         >
-          <div className="bg-blue-500/50 p-3">component 1</div>
-          <div className="bg-blue-500/50 p-3">component 1</div>
+          <Button>Default Primary</Button>
+          <Button disabled>Disabled Primary</Button>
+          <Button
+            colors={{
+              bg: "bg-amber-500",
+              hoverBg: "hover:bg-amber-600",
+              focusRing: "focus:ring-amber-500",
+            }}
+          >
+            Amber Primary
+          </Button>
+
+          <Button
+            colors={{
+              bg: "bg-rose-500",
+              hoverBg: "hover:bg-rose-600",
+              focusRing: "focus:ring-rose-500",
+            }}
+          >
+            Rose
+          </Button>
+
+          <Button
+            colors={{
+              bg: "bg-fuchsia-500",
+              hoverBg: "hover:bg-fuchsia-600",
+              focusRing: "focus:ring-fuchsia-500",
+            }}
+          >
+            Fuchsia
+          </Button>
+
+          <Button
+            colors={{
+              bg: "bg-indigo-500",
+              hoverBg: "hover:bg-indigo-600",
+              focusRing: "focus:ring-indigo-500",
+            }}
+          >
+            Indigo
+          </Button>
+
+          <Button
+            colors={{
+              bg: "bg-[#1da1f2]",
+              hoverBg: "hover:bg-[#1a8cd8]",
+              focusRing: "focus:ring-[#1da1f2]",
+            }}
+          >
+            Twitter Blue
+          </Button>
+        </Section>
+
+        {/* BUTTON SECONDARY */}
+        <Section title="Secondary" description="Secondary button blablablal">
+          <Button variant="secondary">Default Secondary</Button>
+          <Button variant="secondary" disabled>
+            Default Secondary
+          </Button>
+
+          <Button
+            variant="secondary"
+            colors={{
+              text: "text-amber-500",
+              hoverBorder: "hover:border-amber-500",
+              hoverBg: "hover:bg-amber-500",
+              focusRing: "focus:ring-amber-500",
+            }}
+          >
+            Amber Secondary
+          </Button>
+
+          <Button
+            variant="secondary"
+            colors={{
+              text: "text-rose-500",
+              hoverBorder: "hover:border-rose-500",
+              hoverBg: "hover:bg-rose-500",
+              focusRing: "focus:ring-rose-500",
+            }}
+          >
+            Rose
+          </Button>
+
+          <Button
+            variant="secondary"
+            colors={{
+              text: "text-fuchsia-500",
+              hoverBorder: "hover:border-fuchsia-500",
+              hoverBg: "hover:bg-fuchsia-500",
+              focusRing: "focus:ring-fuchsia-500",
+            }}
+          >
+            Fuchsia
+          </Button>
+
+          <Button
+            variant="secondary"
+            colors={{
+              text: "text-indigo-500",
+              hoverBorder: "hover:border-indigo-500",
+              hoverBg: "hover:bg-indigo-500",
+              focusRing: "focus:ring-indigo-500",
+            }}
+          >
+            Indigo
+          </Button>
+
+          <Button
+            variant="secondary"
+            colors={{
+              text: "text-[#1da1f2]",
+              hoverBorder: "hover:border-[#1da1f2]",
+              hoverBg: "hover:bg-[#1da1f2]",
+              focusRing: "focus:ring-[#1da1f2]",
+            }}
+          >
+            Twitter blue
+          </Button>
+        </Section>
+
+        {/* BUTTON TERTIARY */}
+        <Section title="Tertiary" description="Tertiary button blablablal">
+          <Button variant="tertiary">Default tertiary</Button>
+          <Button variant="tertiary" disabled>
+            Default tertiary
+          </Button>
+
+          <Button
+            variant="tertiary"
+            colors={{
+              text: "text-amber-500",
+              hoverText: "hover:text-amber-700",
+              focusRing: "focus:ring-amber-500",
+            }}
+          >
+            Amber Secondary
+          </Button>
+
+          <Button
+            variant="tertiary"
+            colors={{
+              text: "text-rose-500",
+              hoverText: "hover:text-rose-700",
+              focusRing: "focus:ring-rose-500",
+            }}
+          >
+            Rose
+          </Button>
+
+          <Button
+            variant="tertiary"
+            colors={{
+              text: "text-fuchsia-500",
+              hoverText: "hover:text-fuchsia-700",
+              focusRing: "focus:ring-fuchsia-500",
+            }}
+          >
+            Fuchsia
+          </Button>
+
+          <Button
+            variant="tertiary"
+            colors={{
+              text: "text-indigo-500",
+              hoverText: "hover:text-indigo-700",
+              focusRing: "focus:ring-indigo-500",
+            }}
+          >
+            Indigo
+          </Button>
+
+          <Button
+            variant="tertiary"
+            colors={{
+              text: "text-[#1da1f2]",
+              hoverText: "hover:text-[#1da1f2]",
+              focusRing: "focus:ring-[#1da1f2]",
+            }}
+          >
+            Twitter blue
+          </Button>
         </Section>
       </Container>
     </Layout>
---
+++ src/components/library/Button.tsx
@@ -0,0 +1,81 @@
+import { twMerge } from "tailwind-merge";
+
+// COLORS TYPE
+type PrimaryColors = {
+  bg?: string;
+  hoverBg?: string;
+  focusRing?: string;
+};
+
+type SecondaryColors = {
+  text?: string;
+  hoverBorder?: string;
+  hoverBg?: string;
+  focusRing?: string;
+};
+
+type TertiaryColors = {
+  text?: string;
+  hoverText?: string;
+  focusRing?: string;
+};
+
+type BaseButtonProps = React.ButtonHTMLAttributes<HTMLButtonElement>;
+
+// Overload Signatures for the button component
+type ButtonComponent = {
+  (
+    props: BaseButtonProps & { variant?: "primary"; colors?: PrimaryColors }
+  ): JSX.Element;
+  (
+    props: BaseButtonProps & { variant: "secondary"; colors?: SecondaryColors }
+  ): JSX.Element;
+  (
+    props: BaseButtonProps & { variant: "tertiary"; colors?: TertiaryColors }
+  ): JSX.Element;
+};
+
+export const Button: ButtonComponent = ({
+  variant = "primary",
+  colors = {},
+  ...props
+}) => {
+  // base clasess
+  const baseClasses: string =
+    "inline-flex items-center justify center gap-2 rounded-md px-4 text-sm font-medium transition-all focus:outline-none focus:ring-2 focus:ring-offset-2 disabled:pointer-events-none disabled:opacity-50";
+
+  const generateVariantClasses = (variant: string, colorProps: any) => {
+    switch (variant) {
+      case "primary":
+        return `border border-transparent py-3 text-white ${
+          colorProps.bg || "bg-emerald-500"
+        } ${colorProps.hoverBg || "hover:bg-emerald-600"} ${
+          colorProps.focusRing || "focus:ring-emerald-500"
+        }`;
+
+      case "secondary":
+        return `border-2 border-gray-200 py-[0.688rem] hover:text-white ${
+          colorProps.text || "text-emerald-500"
+        } ${colorProps.hoverBorder || "hover:border-emerald-500"} ${
+          colorProps.hoverBg || "hover:bg-emerald-500"
+        } ${colorProps.focusRing || "focus:ring-emerald-500"}`;
+
+      case "tertiary":
+        return `border border-tranparent py-3 underline-offset-4 ring-offset-white hover:underline ${
+          colorProps.text || "text-emerald-500"
+        } ${colorProps.hoverText || "hover:text-emerald-700"} ${
+          colorProps.focusRing || "focus:ring-emerald-500"
+        }`;
+      default:
+        return "";
+    }
+  };
+
+  // Generate variant classses base ont the extracted color pros
+  const variantClasses = generateVariantClasses(variant, colors);
+
+  // Merge the base classes with the variant classes and any additional classes
+  const mergedClasses = twMerge([baseClasses, variantClasses]);
+
+  return <button className={mergedClasses} {...props} />;
+};
